b6de967e7e49082f0e2c71c3ffa603f4f6743564
shreeya <pshreeya050@gmail.com>
2025-08-02 17:36:49 +0530

    new

--- server/models/User.js
+++ server/models/User.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     recentSearchedCities: {type: String,  default: []},
 }, { timestamps: true });
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User",userSchema); 
 
 export default User;
 
