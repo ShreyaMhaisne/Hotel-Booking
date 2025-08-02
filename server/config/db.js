b8de0ef3cc112db6ee03b54d31c140ad8e349d1f
shreeya <pshreeya050@gmail.com>
2025-08-02 16:15:54 +0530

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-   if (mongoose.connection.readyState >= 1) return;
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI );
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
